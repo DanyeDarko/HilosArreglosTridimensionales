90e27b9b24b85f53f1adfd84d24a8627dc48a9cc
daniel <danielbrzen@outlook.com>
2020-01-22 12:59:16 -0600

    Interfaz que implementara  clase abstracta monitor

--- src/Interfaces/InterfazMonitorArreglo.java
+++ src/Interfaces/InterfazMonitorArreglo.java
@@ -2,5 +2,7 @@
 package Interfaces;
 
 public interface InterfazMonitorArreglo {
-    
+    public int[][] generarNumeros();
+    public void imprimirArreglo();
+    public  void decidirOperacion(int operacion);
 }
