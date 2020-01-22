641cf0ace070d5ceb0b18119f69bcfb6aa8f915d
daniel <danielbrzen@outlook.com>
2020-01-22 12:59:31 -0600

    Hilo Para realizar Operaciones

--- src/Hilos/hilosOperacion.java
+++ src/Hilos/hilosOperacion.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hilos;
 
-/**
- *
- * @author ye
- */
-public class hilosOperacion {
-    
+
+public class hilosOperacion extends Thread{
+   
+    public void run (){
+
+    }
 }
