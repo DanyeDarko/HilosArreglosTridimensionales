b401b82a400bd6e3d1e433273fe6e542f289d324
daniel <danielbrzen@outlook.com>
2020-01-27 12:08:30 -0600

    hilo funcional con switch para Operaciones

--- src/Hilos/hilosOperacion.java
+++ src/Hilos/hilosOperacion.java
@@ -1,7 +1,7 @@
 
 package Hilos;
 
-import com.oracle.webservices.internal.api.databinding.DatabindingMode;
+
 
 import Objetos.monitorHilos;
 
@@ -9,36 +9,36 @@ public class hilosOperacion extends Thread{
    
     monitorHilos Datos;
     int operacion;
-    int filas,columnas;
+    int tamañoArreglo;
 
 
     public hilosOperacion(monitorHilos d,int operacion){
     this.Datos=d;
     this.operacion=operacion;
-    this.filas=Datos.getTamañoArreglo();
-    this.columnas=Datos.getTamañoArreglo();
+    this.tamañoArreglo=Datos.getTamañoArreglo();
+   
     }
 
 
     public void run (){
-        System.out.println("LA OPERACION ES "+operacion);
         switch (operacion) {
             case 1:
-              Datos.decidirOperacion(operacion, 0, (filas / 2), 0, (columnas / 2), 1);
+              Datos.decidirOperacion(operacion, 0, (tamañoArreglo/ 2), 0, (tamañoArreglo / 2), operacion);
                 break;
             case 2:
-                Datos.decidirOperacion(operacion, (columnas / 2), columnas, 0, (filas / 2), 1);
+            Datos.decidirOperacion(operacion, 0, (tamañoArreglo/2), (tamañoArreglo / 2), tamañoArreglo, operacion);
                 break;
             case 3:
-                Datos.decidirOperacion(operacion, 0, (filas / 2), (filas / 2), filas, 1);
+            Datos.decidirOperacion(operacion, (tamañoArreglo / 2), tamañoArreglo, 0, (tamañoArreglo/ 2), operacion);
                 break;
             case 4:
-                Datos.decidirOperacion(operacion, (columnas / 2), columnas, (filas / 2), filas, 1);
+            Datos.decidirOperacion(operacion,(tamañoArreglo / 2), tamañoArreglo, (tamañoArreglo / 2), tamañoArreglo, operacion);
                 break;
             case 5:
-                Datos.operacionCentral();
+                Datos.operacionCentral(operacion);
                 break;
         }
 
     }
 }
+
